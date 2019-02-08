0ca1fd2d0537bb2bc4b813ae41f0d2e1477dabcf
Arjun Banerjee <arj.banerjee@yahoo.co.uk>
2019-02-08 17:39:13 +0000

    Redesigned, introduced some new images, added a footer

--- Final Version/assets/js/scrolling-nav.js
+++ Final Version/assets/js/scrolling-nav.js
@@ -8,8 +8,8 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: (target.offset().top - 56)
-        }, 2000, "easeInOutExpo");
+          scrollTop: (target.offset().top - 170)
+        }, 1000, "easeInOutExpo");
         return false;
       }
     }
